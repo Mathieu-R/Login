7de2c861cc5e467e50b546f84d0aab9d47cabc9b
Mathieu <mathieu0709@gmail.com>
2016-03-30 14:11:54 +0200

    WebSocket

--- server/chat.js
+++ server/chat.js
@@ -2,19 +2,38 @@ var express = require("express"); // Routes
 var app = express();
 var http = require("http");
 var server = http.Server(app);
-var io = require("socket.io")(server); // Socket io
+var wsServer = require("ws").server;
+//var io = require("socket.io")(server); // Socket io
 
 console.log("chat.js");
+
+var ws = new wsServer({
+  httpServer: server,
+  port: 8080
+});
+
+ws.onopen = function(e) {
+  console.log("Connecté au serveur");
+
+  ws.onmessage = function(data) {
+    ws.send(data);
+  }
+}
+
+ws.onclose = function(e) {
+  console.log(e);
+  console.log("Déconnecté du serveur");
+}
 //alert("chat.js");
 
-io.on("connection", function(socket) {
+/*io.on("connection", function(socket) {
   console.log("User connected");
 
   socket.on("connect", function(data){
-    /*console.log("ok");
+    console.log("ok");
     console.log("user");
     console.log("gravatar");
-    console.log(data.user + "est connecté");*/
+    console.log(data.user + "est connecté");
     socket.emit("newUser", data);
   });
 
@@ -23,4 +42,4 @@ io.on("connection", function(socket) {
     socket.emit("newMessage", message)
   });
 
-});
+});*/
